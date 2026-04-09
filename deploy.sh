73c5ab7e529e09f84dab46edc048afa3e163ab37
dextereskalarte <61570101+hunt3rXhunt3r@users.noreply.github.com>
2026-04-09 15:05:20 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,12 @@
-#!/bin/bash
+# Pumasok sa folder
+cd ~/mtkovpn_gcp
 
-# 1. Siguraduhin na ang PROJECT_ID ay kukuha sa active account mo sa Cloud Shell
-# Para hindi mo na kailangang i-type manually.
+# I-deploy ulit (Siguraduhin na ang PROJECT_ID ay tama)
 PROJECT_ID=$(gcloud config get-value project)
+SERVICE_NAME="mtkovpn-dns-new"
 
-# 2. Ang SERVICE_NAME ay dapat walang underscore (_). Gagamit tayo ng hyphen (-).
-SERVICE_NAME="mtkovpn-gcp-dns"
-REGION="asia-southeast1"
-
-echo "Deploying to Project: $PROJECT_ID"
-
-# 1. I-build ang Docker image gamit ang Cloud Build (mas mabilis sa GCP)
-# Ito ay mas reliable kaysa sa 'docker build' sa loob ng Cloud Shell
 gcloud builds submit --tag gcr.io/$PROJECT_ID/$SERVICE_NAME
 
-# 2. I-deploy sa Cloud Run
 gcloud run deploy $SERVICE_NAME \
   --image gcr.io/$PROJECT_ID/$SERVICE_NAME \
   --platform managed \
